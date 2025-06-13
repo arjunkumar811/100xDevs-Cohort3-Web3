c536afcb5930333cd87c171d19011f709d9b5512
arjunkumar811 <kumararjun26401@gmail.com>
2025-06-13 16:51:21 +0530

    Added Code

--- Week 01 - Orientation/1.1 - Orientation Web3/Lecture Codes/index.js
+++ Week 01 - Orientation/1.1 - Orientation Web3/Lecture Codes/index.js
@@ -3,3 +3,8 @@ const crypto = require('crypto');
 const input = "100xdev";
 const hash = crypto.createHash('sha256').update(input).digest('hex');
 console.log(hash);
+
+
+const input1 = "Arjun";
+const hash1 = crypto.createHash('sha256').update(input1).digest('hex');
+console.log(hash1);
