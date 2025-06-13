1ca01edc73cd3a19cc236130905daf72efa6fdf5
arjunkumar811 <kumararjun26401@gmail.com>
2025-06-13 18:34:19 +0530

    Added Code

--- Week 01 - Orientation/1.1 - Orientation Web3/Assignment Solution/Assignment 1 Solution/index.js
+++ Week 01 - Orientation/1.1 - Orientation Web3/Assignment Solution/Assignment 1 Solution/index.js
@@ -3,20 +3,19 @@
 const crypto = require('crypto');
 
 
-// Function to find an input string that produces a hash starting with '00000'
-function findHashWithPrefix(prefix) {
-    let input = 0;
-    while (true) {
-        let inputStr = input.toString();
-        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
-        if (hash.startsWith(prefix)) {
-            return { input: inputStr, hash: hash };
+const findHashWithPrefix = (prefix) => {
+    let nonce = 0;
+    while(true) {
+        const input = `Arjun${nonce}`;  // Arjun384176
+        const hash = crypto.createHash('sha256').update(input).digest('hex');
+        if(hash.startsWith(prefix)) {
+            console.log(`Found input: ${input}`);  // Arjun384176
+            console.log(`Hash: ${hash}`);  // 0000032c01e53ec9745e7ea0e7f598a8e820961f7931cf9fe387e50edd28461a
+            break;
         }
-        input++;
+        nonce++;
     }
 }
 
-// Find and print the input string and hash
-const result = findHashWithPrefix('00000');
-console.log(`Input: ${result.input}`);
-console.log(`Hash: ${result.hash}`);
+const prefix = '00000';   
+findHashWithPrefix(prefix);
