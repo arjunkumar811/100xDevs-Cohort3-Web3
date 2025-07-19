507794bad9486d40ed06f3628a902d4934c83128
arjunkumar811 <kumararjun26401@gmail.com>
2025-07-19 16:44:46 +0530

    UInt8Array to ascii

--- Week 02 - Public key Crypography/2.2 - Public Key Cryptography - Offline (From scratch)/index.js
+++ Week 02 - Public key Crypography/2.2 - Public Key Cryptography - Offline (From scratch)/index.js
@@ -24,3 +24,16 @@ function asciiToBytes(asciiString) {
 const ascii = "Hello";
 const byteArray = asciiToBytes(ascii);
 console.log(byteArray); // Output: [72, 101, 108, 108, 111]
+
+
+
+
+// 3. UInt8Array to ascii
+function bytesToAscii(byteArray) {
+  return new TextDecoder().decode(byteArray);
+}
+
+// Example usage:
+const bytes = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
+const asciiString = bytesToAscii(bytes);
+console.log(asciiString); // Output: "Hello"
