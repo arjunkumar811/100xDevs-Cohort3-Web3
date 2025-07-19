99ecf022ffed5d0150819b1604cdd9ba0df66d00
arjunkumar811 <kumararjun26401@gmail.com>
2025-07-19 18:20:11 +0530

    Added solona

--- Week 02 - Public key Crypography/2.2 - Public Key Cryptography - Offline (From scratch)/index.sol
+++ Week 02 - Public key Crypography/2.2 - Public Key Cryptography - Offline (From scratch)/index.sol
@@ -24,3 +24,63 @@ const result = nacl.sign.detached.verify(
 );
 
 console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+// Using @noble/ed25519
+import * as ed from "@noble/ed25519";
+
+async function main() {
+  // Generate a secure random private key
+  const privKey = ed.utils.randomPrivateKey();
+
+  // Convert the message "hello world" to a Uint8Array
+  const message = new TextEncoder().encode("hello world");
+
+  // Generate the public key from the private key
+  const pubKey = await ed.getPublicKeyAsync(privKey);
+
+  // Sign the message
+  const signature = await ed.signAsync(message, privKey);
+
+  // Verify the signature
+  const isValid = await ed.verifyAsync(signature, message, pubKey);
+
+  // Output the result
+  console.log(isValid); // Should print `true` if the signature is valid
+}
+
+main();
+
+
+
+
+
+
+
+Using @noble/secp256k1
+import * as secp from "@noble/secp256k1";
+
+async function main() {
+  const privKey = secp.utils.randomPrivateKey(); // Secure random private key
+  // sha256 of 'hello world'
+  const msgHash =
+    "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9";
+  const pubKey = secp.getPublicKey(privKey);
+  const signature = await secp.signAsync(msgHash, privKey); // Sync methods below
+  const isValid = secp.verify(signature, msgHash, pubKey);
+  console.log(isValid);
+}
+
+main();
+
+
