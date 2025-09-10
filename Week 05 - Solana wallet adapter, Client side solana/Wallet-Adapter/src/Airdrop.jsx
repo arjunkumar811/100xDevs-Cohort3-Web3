c1d41a81c92cfb282f779ab35e09e152258b5384
arjunkumar811 <kumararjun26401@gmail.com>
2025-09-10 22:33:47 +0530

    Airdrop

---
+++ Week 05 - Solana wallet adapter, Client side solana/Wallet-Adapter/src/Airdrop.jsx
@@ -0,0 +1,5 @@
+export function Airdrop() {
+    return <div>
+        hi from air drop
+    </div>
+}
--- Week 05 - Solana wallet adapter, Client side solana/Wallet-Adapter/src/App.jsx
+++ Week 05 - Solana wallet adapter, Client side solana/Wallet-Adapter/src/App.jsx
@@ -11,6 +11,7 @@ import { clusterApiUrl } from '@solana/web3.js';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
+import { Airdrop } from './Airdrop';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
   return (
     <>
      hi there
+     <Airdrop/>
     </>
   )
 }
