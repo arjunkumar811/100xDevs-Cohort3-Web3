cf996f2e566604dece08eddbe5a2a93cbb32c65b
arjunkumar811 <kumararjun26401@gmail.com>
2025-10-05 22:18:09 +0530

    Added Providers

--- Week 11 - ETH Adapters/ETHWallet/src/App.jsx
+++ Week 11 - ETH Adapters/ETHWallet/src/App.jsx
@@ -1,26 +1,39 @@
-import { createPublicClient, http } from 'viem'
-import { mainnet } from 'viem'
-
-const client = createPublicClient({
-  chain: mainnet,
-  transport: http(),
-})
-
-
-
-function App() {
+import { createPublicClient, http } from 'viem';
+import './App.css'
+import { QueryClient, QueryClientProvider, useQuery, useQueryClient } from '@tanstack/react-query';
+import { mainnet } from 'viem/chains';
 
 async function getBalance() {
-const res = await client.getBalance("");
-console.log(res);
+  const client = createPublicClient({ 
+    chain: mainnet, 
+    transport: http(), 
+  });
+
+  const balance = await client.getBalance({address: "0x075c299cf3b9FCF7C9fD5272cd2ed21A4688bEeD"}) 
+  return balance.toString();
 }
 
+const queryClient = new QueryClient()
+
+function App() {
   return (
-    <>
-   
-  <button onClick={getBalance}>Get Balance</button>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Todos />
+    </QueryClientProvider>
   )
 }
+
+function Todos() {
+  // Queries
+  const query = useQuery({ queryKey: ['balance'], queryFn: getBalance ,refetchInterval: 10 * 1000 })
+
+  return (
+    <div>
+      Balance: 
+      {query.data}
+    </div>
+  )
+}
+
 
 export default App
