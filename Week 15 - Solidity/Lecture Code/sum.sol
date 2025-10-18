3606198e1b6e4cc86142619e0089a3b39aa40b52
arjunkumar811 <kumararjun26401@gmail.com>
2025-10-18 16:06:43 +0530

    Variables

---
+++ Week 15 - Solidity/Lecture Code/sum.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;  // version of  Solidity compiler
+
+contract Sum {
+    // Unsigned Number
+    uint8 num1 = 0;
+    unit16 bigNum = 12000;
+    unit256 larNum = 100000000; 
+
+    // Signed Number
+    int y = -50000;
+    int32 x = -100;
+
+    // Booleans
+    bool isActive = true;
+
+    // Addresses
+    address arjun = GLue9GG4hHPtq5NWyXhs6XZWriYNG4NRKfXYZac5WFKL;
+
+    // strings
+    string name = "Arjun";
+}
