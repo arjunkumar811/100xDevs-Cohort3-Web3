763efd9f1fe7c42ffe92d1523956038508d42b97
arjunkumar811 <kumararjun26401@gmail.com>
2025-10-25 19:33:18 +0530

    ERC20 Extends

--- Week 18 - Hardhat, Ganache, Foundry/hello_template/src/ArjunCoin.sol
+++ Week 18 - Hardhat, Ganache, Foundry/hello_template/src/ArjunCoin.sol
@@ -6,10 +6,17 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract ArjunCoin is ERC20 {
-
+uint owner;
 
 constructor(uint256 _initialValue) ERC20("Arjun", "AR") {
     
+    _mint(msg.sender, _initialValue);
+    owner = msg.sender;
+}
+
+function mint(address to, uint256 amount) public {
+    require(msg.sender == owner);
+    _mint(to, amount);
 }
 
 
