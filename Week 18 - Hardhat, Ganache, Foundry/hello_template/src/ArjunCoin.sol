f70f4ed0a176b456f3d83a497308fe24991b83ff
arjunkumar811 <kumararjun26401@gmail.com>
2025-10-25 19:15:28 +0530

    Erc20

---
+++ Week 18 - Hardhat, Ganache, Foundry/hello_template/src/ArjunCoin.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+
+contract ArjunCoin is ERC20 {
+
+
+constructor(uint256 _initialValue) ERC20("Arjun", "AR") {
+    
+}
+
+
+ }
