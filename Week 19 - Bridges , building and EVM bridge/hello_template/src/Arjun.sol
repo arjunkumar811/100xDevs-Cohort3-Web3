683b0da38570454a69d0b7a7c2b97e464a9a4f68
arjunkumar811 <kumararjun26401@gmail.com>
2025-10-27 22:28:00 +0530

    Added ERC20

---
+++ Week 19 - Bridges , building and EVM bridge/hello_template/src/Arjun.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract Arjun is ERC20, Ownable {
+
+    constructor() ERC20("Arjun", "AR") Ownable(msg.sender) {
+
+    }
+
+
+    function mint(address _to, uint256 _amount) public onlyOwner {
+    _mint(_to, _amount);
+    }
+
+}
