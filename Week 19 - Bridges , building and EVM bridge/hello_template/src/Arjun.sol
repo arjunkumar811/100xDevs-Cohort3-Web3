50fbdec7615b9571da3fc66f4045981e3b3821e7
arjunkumar811 <kumararjun26401@gmail.com>
2025-10-28 18:31:49 +0530

    Added Lock Contrct

--- Week 19 - Bridges , building and EVM bridge/hello_template/src/Arjun.sol
+++ Week 19 - Bridges , building and EVM bridge/hello_template/src/Arjun.sol
@@ -1,19 +1,31 @@
+// Assignment - Build a contract that lets me lock my erc-20 tokens somewhere. 
+
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract Arjun is ERC20, Ownable {
-
-    constructor() ERC20("Arjun", "AR") Ownable(msg.sender) {
-
-    }
+contract LockToken {
+    address public TokenAddress;
+    mapping(address => uint) pendingBalance;
 
 
-    function mint(address _to, uint256 _amount) public onlyOwner {
-    _mint(_to, _amount);
-    }
+constructor(address _TokenAddress) {
+ TokenAddress = _TokenAddress;
+}
+
+function deposit(uint amount) public {
+require(IERC20(TokenAddress).allowance(msg.sender, address(this)) >= amount);
+IERC20(TokenAddress).transferFrom(msg.sender, address(this), amount);
+pendingBalance[msg.sender] += amount;
+}
+
+function withdraw() public {
+    uint remainBalance = pendingBalance[msg.sender];
+IERC20(TokenAddress).transfer(msg.sender, remainBalance);
+pendingBalance[msg.sender] = 0;
+}
 
 }
