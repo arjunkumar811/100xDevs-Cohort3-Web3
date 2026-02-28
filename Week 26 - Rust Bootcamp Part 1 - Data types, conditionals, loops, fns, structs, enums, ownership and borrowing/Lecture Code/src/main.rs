d42d541a4d206703f4db9fe9590313783f58a97b
arjunkumar811 <kumararjun26401@gmail.com>
2026-02-28 18:45:32 +0530

    function

--- Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing/Lecture Code/src/main.rs
+++ Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing/Lecture Code/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
+    let ans: u16 = sum(1, 2);
+    println!("{}", ans);
 }
+
+fn sum(a: u16, b: u16) -> u16 {
+    return a + b;
+}
+
+// signed number u32
+// unsigned number i32
