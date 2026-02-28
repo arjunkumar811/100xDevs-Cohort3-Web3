c38f22db79c198bfbb7add704e13196a81fb88d6
arjunkumar811 <kumararjun26401@gmail.com>
2026-02-28 18:51:35 +0530

    bool

--- Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing/Lecture Code/src/main.rs
+++ Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing/Lecture Code/src/main.rs
@@ -1,4 +1,4 @@
-fn main() {
+/* fn main() {
     println!("Hello, world!");
     let ans: u16 = sum(1, 2);
     println!("{}", ans);
@@ -10,3 +10,17 @@ fn sum(a: u16, b: u16) -> u16 {
 
 // signed number u32
 // unsigned number i32
+
+*/
+
+
+//Bool
+
+fn main() {
+    let ans: bool = is_even(10);
+    println!("{}", ans);
+}
+
+fn is_even (a: u32) -> bool {
+    return a % 2 == 0;
+}
