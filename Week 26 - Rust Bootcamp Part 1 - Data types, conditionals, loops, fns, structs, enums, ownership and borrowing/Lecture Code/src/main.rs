bf0b9cc7f092cb2c60a92b5598872d9b91ae51a9
arjunkumar811 <kumararjun26401@gmail.com>
2026-04-07 21:24:56 +0530

    Added Code

--- Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing/Lecture Code/src/main.rs
+++ Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing/Lecture Code/src/main.rs
@@ -20,7 +20,7 @@ fn sum(a: u16, b: u16) -> u16 {
     let x: i32 = 1;
     println!("{}", x);
 }
-*/
+*/ 
 
 
 
@@ -54,14 +54,88 @@ fn sum(a: u16, b: u16) -> u16 {
 // Strings
 
 
-fn main() {
-    let num = String::from("Arjun");
-    println!("{}", num);
-}
+// fn main() {
+//     let num = String::from("Arjun");
+//     println!("{}", num);
+// }
+
+
+//  Arrays
+
+// fn main () {
+//     let arr: [i32; 4] = [12, 23, 34, 45];
+//     println!("{}", arr.len());
+    
+// }
 
 
 
+// Vectors
 
+// fn main() {
+//     let vec = vec![12, 23, 34];
+//     print!("{}", xs.len());
+// }
 
 
     
+
+
+
+// Condition
+
+// pub fn main() {
+
+//     let x:i32 = 99;
+//     let is_even = is_even(x);
+//     if is_even {
+//     println!("{} is even" ,x);
+//     } else {
+//     println!("{} is odd" ,x);
+//     }
+// }
+
+// pub fn is_even(x: i32) -> bool{
+//  return x % 2 == 0;
+// }
+
+
+
+// Loops
+
+// fn main() {
+//     for i in 0..5 {
+//         println!("i: {}", i);
+//     }
+// }
+
+
+// Loop with Index
+
+// fn main() {
+//     let arr = [5, 10, 15];
+
+//     for i in 0..arr.len() {
+//         println!("{}", arr[i]);
+//     }
+// }
+
+
+// Loops for Strigs
+
+// pub fn main() {
+//     let str = String::from("harkirat singh");
+//     println!("First name {}", get_first_name(str))
+    
+// }
+
+// pub fn get_first_name(str: String) -> String {
+//     let mut first_name = String::from("");
+//     for c in str.chars() {
+//         if c == ' ' {
+//             break
+//         }
+//         first_name.push(c);
+//     }
+//     return first_name;
+// }
--- Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing/Lecture Code/target/debug/deps/lecture_code-9fe1f285c3c427c3.d
+++
@@ -1,5 +0,0 @@
-C:\Users\HP\Desktop\100xDevs-Cohort3-Web3\Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing\Lecture Code\target\debug\deps\lecture_code-9fe1f285c3c427c3.d: src\main.rs
-
-C:\Users\HP\Desktop\100xDevs-Cohort3-Web3\Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing\Lecture Code\target\debug\deps\lecture_code-9fe1f285c3c427c3.exe: src\main.rs
-
-src\main.rs:
