c081dec747e9f6083c4f8533b62160077cabbe5a
arjunkumar811 <kumararjun26401@gmail.com>
2026-04-07 19:19:19 +0530

    Added String

--- Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing/Lecture Code/src/main.rs
+++ Week 26 - Rust Bootcamp Part 1 - Data types, conditionals, loops, fns, structs, enums, ownership and borrowing/Lecture Code/src/main.rs
@@ -14,29 +14,54 @@ fn sum(a: u16, b: u16) -> u16 {
 
 */
 
-// NUmbers
+// Nmbers
 
-fn main() {
+/*fn main() {
     let x: i32 = 1;
     println!("{}", x);
 }
+*/
 
 
 
+// What happens if we overflow?
 
-
+/*fn main() {
+    let mut num: i8 = 124;
+    for i in 0..100 {
+        num += 127;
+    }
+    print!("Number: {}", num)
+}
+*/
 
 
 
 //Bool
-/* 
+
+
+// fn main() {
+//     let ans: bool = is_even(10);
+//     println!("{}", ans);
+// }
+
+// fn is_even (a: u32) -> bool {
+//     return a % 2 == 0;
+// }
+
+
+
+// Strings
+
+
 fn main() {
-    let ans: bool = is_even(10);
-    println!("{}", ans);
+    let num = String::from("Arjun");
+    println!("{}", num);
 }
 
-fn is_even (a: u32) -> bool {
-    return a % 2 == 0;
-}
 
-    */
+
+
+
+
+    
