e3d001de6504c9a52dbd7a708437f65d87430767
arjunkumar811 <kumararjun26401@gmail.com>
2026-04-11 18:49:00 +0530

    Added Code

--- Week 27 - Rust Bootcamp Part 2/lecture_code/src/main.rs
+++ Week 27 - Rust Bootcamp Part 2/lecture_code/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let str = String::from("Arjun");
+    let len = get_length(str);
+    println!("{}", len);
+
+    print!("{}", str); // gives waring
+}
+
+fn get_length(str: String) -> usize {
+    return str.len()
 }
---
+++ Week 27 - Rust Bootcamp Part 2/lecture_code/target/debug/deps/lecture_code-5658d61286d15fb6.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Desktop\100xDevs-Cohort3-Web3\Week 27 - Rust Bootcamp Part 2\lecture_code\target\debug\deps\lecture_code-5658d61286d15fb6.d: src\main.rs
+
+C:\Users\HP\Desktop\100xDevs-Cohort3-Web3\Week 27 - Rust Bootcamp Part 2\lecture_code\target\debug\deps\liblecture_code-5658d61286d15fb6.rmeta: src\main.rs
+
+src\main.rs:
