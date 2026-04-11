72a6c8564f2ddf6d1ce385251ba744b1b4adc80b
arjunkumar811 <kumararjun26401@gmail.com>
2026-04-11 18:54:14 +0530

    Added Code

--- Week 27 - Rust Bootcamp Part 2/lecture_code/src/main.rs
+++ Week 27 - Rust Bootcamp Part 2/lecture_code/src/main.rs
@@ -9,3 +9,18 @@ fn main() {
 fn get_length(str: String) -> usize {
     return str.len()
 }
+
+
+
+
+//Transferring back ownership 
+fn main() {
+    let str = String::from("Harkirat");
+    let (str, len) = get_length(str);
+    println!("{} {}", str, len);
+}
+
+fn get_length(str: String) -> (String, usize) {
+    let len = str.len();
+    return (str, len);
+}
